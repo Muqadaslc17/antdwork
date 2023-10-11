8a8cb6bc5c972dff74d418dc75f0edd8b75abdfb
Muqadaslc17 <147405232+Muqadaslc17@users.noreply.github.com>
2023-10-11 00:25:25 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 
 import './App.css';
-import { Button } from 'antd';
-import Modalcom from "../src/Modalcom"
 function App() {
   return (
     <>
-  <Modalcom></Modalcom>
   <Buttoncom></Buttoncom>
     </>
     
