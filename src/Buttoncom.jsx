56ecd5a94adb18800a18180fbcb0f9b3530b3530
Muqadaslc17 <147405232+Muqadaslc17@users.noreply.github.com>
2023-10-11 00:49:07 -0700

    added functionality

--- src/Buttoncom.jsx
+++ src/Buttoncom.jsx
@@ -1,13 +1,25 @@
 import './App.css';
 import { Button } from 'antd';
+import { useState } from 'react';
+
+
 function Buttoncom() {
+  const [Loading,setLoading]=useState(false);
+  
   const onBtnClick=(e)=>{
-console.log("i ckikced btn")
-  }
+console.log("i ckikced btn");
+setLoading(true);
+setTimeout (()=>{
+setLoading(false)
+},4000) 
+}
+
+
   return (
     <div className="App">
       <Button type="default"
-      onClick={onBtnClick}>Button</Button>
+      onClick={onBtnClick}
+      loading={Loading}>Button</Button>
     </div>
   );
 }
